89986f88efaa37684b3fe1be2d686b25aad20532
Juan <arzzdeveloper04@gmail.com>
2024-04-10 16:58:22 -0600

    regression-added

---
+++ src/QuadraticLinearRegression.java
@@ -0,0 +1,80 @@
+public class QuadraticLinearRegression {
+
+    private double a, b, c, varR;
+
+    public void fit(double[] x, double[] y) {
+        int n = x.length;
+        DiscreteMaths dm = new DiscreteMaths();
+
+        // Variables Assigner
+        double sumX = dm.sumX(x);
+        double sumY = dm.sumY(y);
+        double sumXY = dm.sumXY(x, y);
+        double sumXSquare = dm.sumXSquare(x);
+        double sumXSquareY = dm.sumXSquareY(x, y);
+        double sumXCube = dm.sumXCubic(x);
+        double sumXQuartic = dm.sumXQuartic(x);
+
+        // Variables Reassigner
+        double newSumXX = (sumXSquare) - (Math.pow(sumX,2)/n);
+        double newSumXY = (sumXY) - ((sumX*sumY)/n);
+        double newSumXXSquare = (sumXCube) - ((sumXSquare*sumX)/n);
+        double newSumXSquareY = (sumXSquareY) - ((sumXSquare*sumY)/n);
+        double newSumXSquareXSquare = (sumXQuartic) - ((Math.pow(sumXSquare, 2))/n);
+
+        double den = (newSumXX * newSumXSquareXSquare) - Math.pow(newSumXXSquare, 2);
+
+        a = ( (newSumXSquareY * newSumXX) - (newSumXY * newSumXXSquare) ) / ( den ) ;
+        b = ( (newSumXY * newSumXSquareXSquare) - (newSumXSquareY * newSumXXSquare) ) / ( den ) ;
+        c = ( sumY - (b * sumX) - (a * sumXSquare) ) /n;
+
+        double rSSE = (sumY - (a* sumXSquare) - (b*sumX - c));
+        double rSST = Math.pow((sumY - (sumY / n)), 2);
+
+        varR = ( 1 - (rSSE/rSST) );
+    }
+
+    public double predict(double x) {
+        return (a * Math.pow(x, 2)) + (b * x) + c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getR() {
+        return varR;
+    }
+
+    public static void calculateQuadraticRegression(double newXQuadratic) {
+        DataSet ds = new DataSet();
+        double[] xData = ds.getX();
+        double[] yData = ds.getY();
+
+        // Calcular la regresión cuadrática
+        QuadraticLinearRegression quadraticRegression = new QuadraticLinearRegression();
+        quadraticRegression.fit(xData, yData);
+
+        // Obtener los coeficientes de la regresión cuadrática
+        double aQuadratic = quadraticRegression.getA();
+        double bQuadratic = quadraticRegression.getB();
+        double cQuadratic = quadraticRegression.getC();
+        double R = quadraticRegression.getR();
+
+        // Predecir el valor de Y para un nuevo valor de X usando regresión cuadrática
+        double predictedYQuadratic = quadraticRegression.predict(newXQuadratic);
+
+        // Imprimir la ecuación de regresión cuadrática
+        System.out.println("Ecuación de regresión cuadrática: Y = (" + aQuadratic + "(a) * " + newXQuadratic + "^2) + (" + bQuadratic + "(b) * X) + " + cQuadratic + "(c)");
+        System.out.println("Predicción cuadrática para X = " + newXQuadratic + ": Y = " + predictedYQuadratic);
+        System.out.println("R = " + R);
+    }
+}
---
+++ src/SimpleLinearRegression.java
@@ -0,0 +1,55 @@
+public class SimpleLinearRegression {
+
+    private double intercept;   // es lo mismo a B0
+    private double slope;       // es lo mismo a B1
+
+    public void fit(double[] x, double[] y) {
+        int n = x.length;
+        DiscreteMaths dm = new DiscreteMaths();
+
+        double sumX = dm.sumX(x);
+        double sumY = dm.sumY(y);
+
+        double sumXY = dm.sumXY(x,y);
+        double sumXSquare = dm.sumXSquare(x);
+
+        // Calcular los coeficientes de la regresión lineaL
+        slope = (n * sumXY - sumX * sumY) / (n * sumXSquare - sumX * sumX); // B1
+        intercept = (sumY - slope * sumX) / n; // B0
+    }
+
+    public double predict(int x) {
+        return  intercept + slope * x;
+    }
+
+    public double getSlope() { // Obtener B1
+        return slope;
+    }
+
+    public double getIntercept() { // Obtener B2
+        return intercept;
+    }
+
+    public static void calculateSimpleLinearRegression(int newXLinear) {
+        DataSet ds = new DataSet();
+        double[] xData = ds.getX();
+        double[] yData = ds.getY();
+
+        // Calcular la regresión lineal simple
+        SimpleLinearRegression linearRegression = new SimpleLinearRegression();
+        linearRegression.fit(xData, yData);
+
+        // Obtener los coeficientes de la regresión lineal
+        double interceptLinear = linearRegression.getIntercept(); // B0
+        double slopeLinear = linearRegression.getSlope(); // B1
+
+        // Predecir el valor de Y para un nuevo valor de X usando regresión lineal
+        double predictedYLinear = linearRegression.predict(newXLinear);
+
+        // Imprimir la ecuación de regresión lineal
+        System.out.println("Ecuación de regresión lineal: ŷ = " + interceptLinear + " (B0) + " + slopeLinear + " (B1) * " + newXLinear + "(X)");
+
+        // Imprimir la prediccion de regresión lineal
+        System.out.println("Predicción lineal para X = " + newXLinear + ": Y = " + predictedYLinear);
+    }
+}
